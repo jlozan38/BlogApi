d440675e60f07a40b0af2e1257aeb0c03c1435ef
Jesus Lozano <jalozano841@gmail.com>
2020-03-27 20:56:10 -0500

    Added Dotenv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello There')
+import './helpers/dotenv'
+
+import express from 'express'
+
+const app = express()
+const port = parseInt(process.env.PORT)
+
+app.get('/', (req, res) => {
+    res.send('Hello There')
+})
+app.listen(port)
