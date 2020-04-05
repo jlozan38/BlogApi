dd6c61310cb0831daa25301c9300434528a94b7e
Jesus Lozano <jalozano841@gmail.com>
2020-04-04 23:37:02 -0500

    Posts Services Done

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -1,22 +1,37 @@
 import { Router } from 'express'
+import * as postService from '../../services/posts'
 
 const router = Router()
 
-router.get('/posts', (req, res) => {
-    res.json({ todo: 'Get All Posts' })
-})
-router.get('/posts/:id', (req, res) => {
-    res.json({ todo: `Get one post with id: ${req.params.id}` })
-})
-router.post('/posts', (req, res) => {
-    res.json({ todo: 'Add one Post' })
-})
-router.put('/posts/:id', (req, res) => {
-    res.json({ todo: `Updating posts with id: ${req.params.id}`  })
-})
-router.delete('/posts/:id', (req, res) => {
-    res.json({ todo: `Deleting posts with id: ${req.params.id}` })
+router.get('/posts', async (req, res) => res.send(await postServices.getAll()))
+
+router.get('/posts/:id', async (req, res) => {
+    const post = await postServices.getById(req.params.id)
+    post ? res.send(post) : res.status(404).end()
 })
 
+router.post('/posts', auth, async (req, res) => {
+    req.body.post ? res.send(await postServices.add(req.body.post)) : res.status(400).send({ msg: '💩 Bad Request' })
+})
+
+router.put('/posts/:id', auth, async (req, res) => {
+    const post = await postServices.getById(req.params.id)
+    req.body.post && post
+        ? post.author.id == req.user.id
+            ? res.send(await postServices.update(req.body.post, req.params.id))
+            : res.status(401).end()
+        : res.status(400).send({ msg: '💩 Bad Request' })
+})
+
+router.delete('/posts/:id', auth, async (req, res) => {
+    const post = await postServices.getById(req.params.id)
+    post
+        ? post.author.id == req.user.id
+            ? res.send(await postServices.remove(req.params.id))
+            : res.status(401).end()
+        : res.status(404).end()
+})
+
+export default router
 
 export default router
